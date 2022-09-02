0aae78ff4b610b795424d8498f9341b813a87a99
Maxim Migalin <max.migalin10@gmail.com>
2022-09-02 16:37:29 +0300

    Creating an array of pizza categories.

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 
 const Categories = () => {
-    return (
-        <div className="categories">
-            <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-            </ul>
-        </div>
-    );
+
+  const [activeItem, setActiveItem] = React.useState(1);
+
+  const onClickCategory = (index) => {
+    setActiveItem(index);
+  };
+
+  const categories = [
+    {id: 1, categories: 'Все'},
+    {id: 2, categories: 'Мясные'},
+    {id: 3, categories: 'Вегетарианская'},
+    {id: 4, categories: 'Гриль'},
+    {id: 5, categories: 'Острые'},
+    {id: 6, categories: 'Закрытые'},
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {
+          categories.map((item) => (
+            <li
+              key={item.id}
+              className={activeItem === item.id ? 'active' : ''}
+              onClick={() => onClickCategory(item.id)}>
+              {item.categories}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 };
 
 export default Categories;
