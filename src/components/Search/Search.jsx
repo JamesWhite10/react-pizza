079697bfc5b90d4ba676693e601285a8cbb6d0de
Max Migalin <80194569+JamesWhite10@users.noreply.github.com>
2023-01-11 21:33:28 +0300

    Add logic search component.

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import searchLogo from '../../assets/search-icon.svg';
+import clearIcon from '../../assets/clear.svg';
 import classNames from './Search.module.scss';
 
-const Search = () => {
+const Search = ({ searchValue, setSearchValue }) => {
+
     return (
         <div className={classNames.root}>
             <input
+                value={searchValue}
+                onChange={(event) => setSearchValue(event.target.value)}
                 placeholder="Найти пиццу..."
                 className={classNames.input}
                 type="text"
@@ -15,6 +19,14 @@ const Search = () => {
                 alt="PizzaBlock logo"
                 className={classNames.logo}
             />
+            {
+                searchValue && <img
+                onClick={() => setSearchValue('')}
+                src={clearIcon}
+                alt="Clear icon"
+                className={classNames.clear}
+            />
+            }
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Sort from "../components/Sort/Sort";
 import PizzaSkeleton from "../components/PizzaSkeleton";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [categoryType, setCategoryType] = React.useState(0);
@@ -18,8 +18,9 @@ const Home = () => {
       const sortBy = sortType.sortProperty.replace('-', '');
       const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
       const category = categoryType > 0 ? `category=${categoryType}` : '';
+      const search = searchValue ? `search=${searchValue}` : '';
 
-    fetch(`https://633f17280dbc3309f3c4b2b3.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(`https://633f17280dbc3309f3c4b2b3.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}&${search}`)
       .then((response) => response.json())
       .then((pizzas) => {
         setLoading(false);
@@ -27,7 +28,12 @@ const Home = () => {
       });
 
     window.scrollTo(0, 0);
-  }, [categoryType, sortType]);
+  }, [categoryType, sortType, searchValue]);
+
+    const skeletons = [...new Array(4)].map((_, index) => (<PizzaSkeleton key={index}/>
+    ))
+
+  const pizzas = items.map((obj) => (<PizzaBlock key={obj.id} {...obj}/>));
 
   return (
     <div className="container">
@@ -37,11 +43,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {loading ? [...new Array(4)].map((_, index) => (
-          <PizzaSkeleton key={index}/>
-        )) : items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj}/>
-        ))}
+        {loading ? skeletons : pizzas}
       </div>
     </div>
   );
